f475aa80e23073f1b038fd854e795f4ca89ce732
Ina Odén Österbo <i-oden@users.noreply.github.com>
2026-05-27 11:31:19 +0200

    test in python

---
+++ python-test.py
@@ -0,0 +1,5 @@
+# this is to check if a spell check also works in python files
+# scilifelab should be flagged
+# SciLifeLab should not be flagged
+# icorrect should be flagged
+# incorrect should not be flagged
